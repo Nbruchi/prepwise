d582a0fe23c1abbdc8b328b6b29106197390aade
Nbruchi <nkundabagenzibruce@gmail.com>
2025-04-04 07:48:22 +0200

    Changed redirect location

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,20 @@
 import {ReactNode} from "react";
 import Link from "next/link";
 import Image from "next/image";
+import {isAuthenticated} from "@/lib/actions/auth.action";
+import {redirect} from "next/navigation";
 
 interface RootLayoutProps{
     children: ReactNode;
 }
 
-const RootLayout = ({children}: RootLayoutProps) => {
+const RootLayout = async ({children}: RootLayoutProps) => {
+    const isUserAuthenticated = await isAuthenticated();
+
+    if (!isUserAuthenticated) {
+        redirect("/sign-in");
+    }
+
     return (
         <div className="root-layout">
             <nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { isAuthenticated } from "@/lib/actions/auth.action";
-import { redirect } from "next/navigation";
+
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
@@ -19,12 +18,6 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isUserAuthenticated = await isAuthenticated();
-  
-  if (!isUserAuthenticated) {
-    redirect("/sign-in");
-  }
-
   return (
     <html lang="en">
       <body className={`${monaSans.className} dark antialiased pattern`}>
